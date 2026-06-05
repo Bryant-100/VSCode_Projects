7687f6ee3fa257f30229cd18efd17c10f24a88aa
Bryant Nguyen <musicdog975@gmail.com>
2026-06-04 23:13:51 -0700

    Recheck all old files and add 2 images to the assets folder

--- digital_art_space/digital_art_space_lab2/test/widget_test.dart
+++ digital_art_space/digital_art_space_lab2/test/widget_test.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_module_1/main.dart';
 
 void main() {
   testWidgets('ArtSpaceApp displays correctly', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const ArtSpaceApp());
+    // Build our app and trigger a frame. Wrap in MaterialApp to provide material bindings (themes, navigation) the widget may expect.
+    await tester.pumpWidget(const MaterialApp(home: ArtSpaceApp()));
 
     expect(find.text('Digital Art Space'), findsOneWidget);
     expect(find.text('Wheat Field with Cypresses'), findsOneWidget);
@@ -12,7 +13,7 @@ void main() {
     expect(find.text('1889'), findsOneWidget);
 
     await tester.tap(find.text('Next'));
-    await tester.pump();
+    await tester.pumpAndSettle();
 
     expect(find.text('Flowers Smile Faces'), findsOneWidget);
     expect(find.text('Takashi Murakami'), findsOneWidget);
